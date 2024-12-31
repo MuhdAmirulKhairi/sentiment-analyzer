37be810b173af85c2c6de6275c2f73eeed4d537e
Amirul Khairi <muhdamirulkhairi.telebort@gmail.com>
2024-12-31 11:00:54 +0800

    commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<section id="header-main" class="p-4">
+   <header>
+      <h1 
+         style="font-family: Roboto, Helvetica, sans-serif"
+         class="d-block text-center m-0">
+         SENTIMENT ANALYZER
+      </h1>
+      <h5 
+         style="font-family: Roboto, Helvetica, sans-serif" 
+         class="d-block text-center m-0">
+         By: Amirul Khairi
+      </h5>
+   </header>
+</section>
+
+<section id="body-main">
+   <div id=main-sidebar>
+      <div id=history-sidebar>
+         <a>Test 1</a>
+      </div>
+      <div id="saved-settings-sidebar">
+         <a>Test 2</a>
+      </div>
+   </div>
+
+   <span></span>
+</section>
+
+<section id="footer-main" class="p-4">
+   <footer>
+      <p
+         style="font-family: Roboto, Helvetica, sans-serif"
+         class="d-block text-center m-0">
+         2024 | Sentiment Analyzer by Amirul Khairi
+      </p>
+   </footer>
+</section>
+
+<style>
+   #header-main {
+      background-color: #8B5DFF;
+   }
+
+   h1 {
+      color: #FFF7D1;
+      -webkit-text-stroke-width: 1px;
+      -webkit-text-stroke-color: #000000;
+      text-shadow: 1px 2px 4px #000000;
+   }
+
+   h5 {
+      color: #FFF7D1;
+      -webkit-text-stroke-width: 0.75px;
+      -webkit-text-stroke-color: #000000;
+      text-shadow: 1px 2px 4px #000000;
+   }
+
+   #footer-main {
+      background-color: #6A42C2;
+   }
+</style>
