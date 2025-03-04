707706c14a73dd7f1377eca1859dfd88a8dfa5d7
MuhdAmirulKhairi <amirulkhairi2002@gmail.com>
2025-03-04 10:43:26 +0800

    Migrated python something2, ran a backend server with admin account.

--- sentiment_python/sentiment_python/settings.py
+++ sentiment_python/sentiment_python/settings.py
@@ -31,18 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'model',
-    'corsheaders'
+    'model'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleWare'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
